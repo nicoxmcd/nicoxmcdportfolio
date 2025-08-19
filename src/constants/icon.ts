f29cab3e036c59228c55a486034ac76f56a638e1
Nicole X McDermott <35404943+nicoxmcd@users.noreply.github.com>
2025-08-19 10:18:16 -0400

    Name change favicon

--- src/constants/icon.ts
+++ src/constants/icon.ts
@@ -2,42 +2,42 @@ import type { Favicon } from "@/types/config.ts";
 
 export const defaultFavicons: Favicon[] = [
 	{
-		src: "/favicon/40/nxm-upper-light.png",
+		src: "/favicon/40/nxa-upper-light.png",
 		theme: "light",
 		sizes: "40x40",
 	},
 	{
-		src: "/favicon/128/nxm-upper-light.png",
+		src: "/favicon/128/nxa-upper-light.png",
 		theme: "light",
 		sizes: "128x128",
 	},
 	{
-		src: "/favicon/180/nxm-upper-light.png",
+		src: "/favicon/180/nxa-upper-light.png",
 		theme: "light",
 		sizes: "180x180",
 	},
 	{
-		src: "/favicon/192/nxm-upper-light.png",
+		src: "/favicon/192/nxa-upper-light.png",
 		theme: "light",
 		sizes: "192x192",
 	},
 	{
-		src: "/favicon/40/nxm-upper-dark.png",
+		src: "/favicon/40/nxa-upper-dark.png",
 		theme: "dark",
 		sizes: "40x40",
 	},
 	{
-		src: "/favicon/128/nxm-upper-dark.png",
+		src: "/favicon/128/nxa-upper-dark.png",
 		theme: "dark",
 		sizes: "128x128",
 	},
 	{
-		src: "/favicon/180/nxm-upper-dark.png",
+		src: "/favicon/180/nxa-upper-dark.png",
 		theme: "dark",
 		sizes: "180x180",
 	},
 	{
-		src: "/favicon/192/nxm-upper-dark.png",
+		src: "/favicon/192/nxa-upper-dark.png",
 		theme: "dark",
 		sizes: "192x192",
 	},
